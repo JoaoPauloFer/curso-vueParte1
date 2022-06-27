3a359115eef6e20a834b34915c90568d3b356141
João Paulo Fer <joaopaulo.data@gmail.com>
2022-06-27 11:57:34 -0300

    docs: 📑  Primeiro commit do projeto

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+
+    <ul>
+      <li :v-for='foto in fotos'>
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+
+      titulo: 'Alurapic',
+      fotos: [
+        {
+          url: "https://blog.farmaciadebicho.com.br/wp-content/uploads/2017/08/cachorro-mergulhando-2.jpg",
+          titulo: 'cachorro'
+        },
+        {
+          url: "https://blog.farmaciadebicho.com.br/wp-content/uploads/2017/08/cachorro-mergulhando-2.jpg",
+          titulo: 'cachorro'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
