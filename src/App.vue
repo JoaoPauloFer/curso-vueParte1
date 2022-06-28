a3692d7e971694c5e4df79a62665e1f507bd6fce
João Paulo Fer <joaopaulo.data@gmail.com>
2022-06-28 09:26:05 -0300

    docs: 📑  Criando componentes reutilizáveis

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
 
-    <ul>
-      <li v-for='foto in fotos'>
-        <img :src="foto.url" :alt="foto.titulo">
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for='foto in fotos'>
+
+        <mue-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" src="foto.url" alt="foto.titulo">
+        </mue-painel>
+
+
       </li>
     </ul>
-
   </div>
 </template>
 
 <script>
+import painel from './component/shared/painel/painel.vue';
 export default {
+
+components: {
+  'mue-painel': painel
+},  
 
 data(){
 
@@ -34,4 +43,25 @@ data(){
 
 <style>
 
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: auto;
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
 </style>
