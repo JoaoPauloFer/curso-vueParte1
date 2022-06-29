085fc666b74e31b42f112a116e343a14dfebbda7
João Paulo Fer <joaopaulo.data@gmail.com>
2022-06-29 09:09:52 -0300

    docs: 📑  Criando pastas Home e Cadastro

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,13 @@
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
 
-    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre por parte do título">
-
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for='foto in fotosComFiltro'>
-
-        <meu-painel :titulo="foto.titulo">
-
-            <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-
-        </meu-painel>
-
-
-      </li>
-    </ul>
   </div>
 </template>
 
 <script>
-import imagemResponsiva  from './component/shared/imagem-responsiva/imagemResponsiva.vue';
-import painel from './component/shared/painel/painel.vue';
+
 export default {
 
-components: {
-  'imagem-responsiva': imagemResponsiva,
-  'meu-painel': painel
-},  
-
-data(){
-
-  return {
-
-      titulo: 'Alurapic',
-      fotos: [],
-      filtro: ''
-    }
-  },
-
-computed: {
-
-  fotosComFiltro() {
-
-    if(this.filtro){
-      let exp = new RegExp(this.filtro.trim(), 'i');
-      return this.fotos.filter(foto => exp.test(foto.titulo))
-    } else {
-      return this.fotos
-    }
-  }
-},
-
-
-created() {
-  this.$http.get('http://localhost:3000/v1/fotos')
-    .then(res => res.json())
-    .then(fotos => this.fotos = fotos, err => console.log(err));
-  }
 }
 </script>
 
@@ -67,22 +17,5 @@ created() {
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: auto;
-  }
-
-  .centralizado {
-    text-align: center;
-  }
-
-  .lista-fotos {
-    list-style: none;
-  }
-
-  .lista-fotos .lista-fotos-item {
-    display: inline-block;
-  }
-
-  .filtro {
-    display: block;
-    width: 100%;
   }
 </style>
